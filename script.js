46c67a899a25c4b91d3b55e202d9201888ad73d8
Seungyoun Lee <syulia9732@gmail.com>
2024-05-15 18:03:08 -0400

    Navbar mobile toggle(incomplete)

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-// JavaScript code goes here
-console.log("Hello from JavaScript!");
+function myFunction() {
+    var links = document.getElementById("mobileLinks");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+        
+    } else {
+      links.style.display = "block";
+      links.style.transform = 'translate(0, 0)';
+    }
+}
+
+const hamMenu = document.querySelector('.hamburger-menu');
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+});
+
+const mobileMenu = document.querySelector("#mobileLinks");
+const mobileMenuBtn = document.querySelector(".hamburger-menu");
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenuBtn.classList.toggle("active-menu-btn");
+    mobileMenu.classList.toggle("slide-out");
+    mobileMenu.classList.toggle("slide-in");
+    if (mobileMenu.classList.contains("slide-out")) {
+      setTimeout(() => {
+        mobileMenu.classList.add("display-none");
+      }, 1000);
+    } else {
+      mobileMenu.classList.remove("display-none");
+    }
+});
+  
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 750) {
+      mobileMenu.classList.add("slide-out");
+      mobileMenu.classList.remove("slide-in");
+      setTimeout(() => {
+        mobileMenu.classList.add("display-none");
+      }, 1000);
+    }
+});
